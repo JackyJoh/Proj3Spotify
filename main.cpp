1bb11007197885a4a5286a16a509fbc45a14fe62
JackyJoh <jackyjoh360@gmail.com>
2025-04-06 16:01:34 -0400

    dynamic array storage updated serial and print

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 struct Song {
-    string serial;
+    int serial;
     string track_id;
     string artist;
     string album_name;
@@ -31,7 +31,7 @@ struct Song {
     string genre;
 
     // Parameterized constructor
-    Song(string serial, string track_id, string artist, string album_name, string track_name,
+    Song(int serial, string track_id, string artist, string album_name, string track_name,
          string popularity, string duration_ms, string explicit_content, string danceability,
          string energy, string key, string loudness, string mode, string speechiness,
          string acousticness, string instrumentalness, string liveness, string valence,
@@ -46,42 +46,44 @@ struct Song {
 
     // Default constructor
     Song()
-        : serial(""), track_id(""), artist(""), album_name(""), track_name(""),
+        : serial(0), track_id(""), artist(""), album_name(""), track_name(""),
           popularity(""), duration_ms(""), explicit_content(""), danceability(""),
           energy(""), key(""), loudness(""), mode(""), speechiness(""),
           acousticness(""), instrumentalness(""), liveness(""), valence(""),tempo(""), time_signature(""), genre("") {}
-    void printSongColumns() {
-        cout << "Song Data Structure Columns:" << endl;
-        cout << "1. serial - Unique identifier for each song" << endl;
-        cout << "2. track_id - Spotify track ID" << endl;
-        cout << "3. artist - Artist name" << endl;
-        cout << "4. album_name - Album title" << endl;
-        cout << "5. track_name - Song title" << endl;
-        cout << "6. popularity - Song popularity rating" << endl;
-        cout << "7. duration_ms - Song duration in milliseconds" << endl;
-        cout << "8. explicit_content - Whether song has explicit content" << endl;
-        cout << "9. danceability - Measure of suitability for dancing" << endl;
-        cout << "10. energy - Measure of intensity and activity" << endl;
-        cout << "11. key - Musical key of the track" << endl;
-        cout << "12. loudness - Overall loudness in decibels" << endl;
-        cout << "13. mode - Major (1) or Minor (0) modality" << endl;
-        cout << "14. speechiness - Presence of spoken words" << endl;
-        cout << "15. acousticness - Amount of acoustic sound" << endl;
-        cout << "16. instrumentalness - Amount of instrumental content" << endl;
-        cout << "17. liveness - Presence of live audience" << endl;
-        cout << "18. valence - Musical positiveness" << endl;
-        cout << "19. tempo - Speed of the track in BPM" << endl;
-        cout << "20. time_signature - Time signature of the track" << endl;
-        cout << "21. genre - Musical genre" << endl;
-    }
+
 };
 
-
+void printSongColumns(Song song) {
+    cout << "1. Serial: " << song.serial << endl;
+    cout << "2. Track ID: " << song.track_id << endl;
+    cout << "3. Artist: " << song.artist << endl;
+    cout << "4. Album Name: " << song.album_name << endl;
+    cout << "5. Track Name: " << song.track_name << endl;
+    cout << "6. Popularity: " << song.popularity << endl;
+    cout << "7. Duration (ms): " << song.duration_ms << endl;
+    cout << "8. Explicit Content: " << song.explicit_content << endl;
+    cout << "9. Danceability: " << song.danceability << endl;
+    cout << "10. Energy: " << song.energy << endl;
+    cout << "11. Key: " << song.key << endl;
+    cout << "12. Loudness: " << song.loudness << endl;
+    cout << "13. Mode: " << song.mode << endl;
+    cout << "14. Speechiness: " << song.speechiness << endl;
+    cout << "15. Acousticness: " << song.acousticness << endl;
+    cout << "16. Instrumentalness: " << song.instrumentalness << endl;
+    cout << "17. Liveness: " << song.liveness << endl;
+    cout << "18. Valence: " << song.valence << endl;
+    cout << "19. Tempo: " << song.tempo << endl;
+    cout << "20. Time Signature: " << song.time_signature << endl;
+    cout << "21. Genre: " << song.genre << endl;
+    cout << "------------------------" << endl;
+}
 
 int main()
 {
-    vector<Song> songList;
+    // vector<Song> songList;
     string line;
+    int index=0;
+    Song* songList = new Song[114000];
     ifstream file("data/dataset.csv");
 
 
@@ -95,44 +97,53 @@ int main()
         Song song;
 
 
+
         // Parse CSV line
         for (int i = 0; i < 21; i++)
         {
 
             string item = (line.substr(0, line.find(",")));
             //cout << item << endl;// Extract the value before the comma
-            line = line.substr(line.find(",") + 1);      // Remove the extracted value from the line
+            line = line.substr(line.find(",") + 1);;// Remove the extracted value from the line
                 switch (i) {
-                case 0: song.track_id = item; break;
-                case 1: song.artist = item; break;
-                case 2: song.album_name = item; break;
-                case 3: song.track_name = item; break;
-                case 4: song.popularity = item; break;
-                case 5: song.duration_ms = item; break;
-                case 6: song.explicit_content = item; break;
-                case 7: song.danceability = item; break;
-                case 8: song.energy = item; break;
-                case 9: song.key = item; break;
-                case 10: song.loudness = item; break;
-                case 11: song.mode = item; break;
-                case 12: song.speechiness = item; break;
-                case 13: song.acousticness = item; break;
-                case 14: song.instrumentalness = item; break;
-                case 15: song.liveness = item; break;
-                case 16: song.valence = item; break;
-                case 17: song.tempo = item; break;
-                case 18: song.time_signature = item; break;
-                case 19: song.genre = item; break;
+                case 0: song.serial = index; break;
+                case 1: song.track_id = item; break;
+                case 2: song.artist = item; break;
+                case 3: song.album_name = item; break;
+                case 4: song.track_name = item; break;
+                case 5: song.popularity = item; break;
+                case 6: song.duration_ms = item; break;
+                case 7: song.explicit_content = item; break;
+                case 8: song.danceability = item; break;
+                case 9: song.energy = item; break;
+                case 10: song.key = item; break;
+                case 11: song.loudness = item; break;
+                case 12: song.mode = item; break;
+                case 13: song.speechiness = item; break;
+                case 14: song.acousticness = item; break;
+                case 15: song.instrumentalness = item; break;
+                case 16: song.liveness = item; break;
+                case 17: song.valence = item; break;
+                case 18: song.tempo = item; break;
+                case 19: song.time_signature = item; break;
+                case 20: song.genre = item; break;
                 default: break;
             }
 
 
         }
-            song.serial = songList.size() + 1; // Assign a serial number
-            songList.push_back(song);
+            // song.serial = songList.size() + 1; // Assign a serial number
+        //cout << index << endl;
+            songList[index] = song;
+        index++;
     }
-    cout << "Successfully loaded " << songList.size() << " songs." << endl;
-    songList[0].printSongColumns();
+    //cout << "Successfully loaded " << songList.length() << " songs." << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        printSongColumns(songList[i]);
+        cout << endl;
+    }
+
 
 
     return 0;
