38ee523c8d34752e3f64e333dde0203b7c10a294
JackyJoh <jackyjoh360@gmail.com>
2025-04-21 12:22:09 -0400

    rebuild

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/jacka/CLionProjects/Proj3Spotify
+# Install script for directory: C:/Users/jacka/CLionProjects/proj3testing
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/jacka/CLionProjects/Proj3Spotify/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/jacka/CLionProjects/proj3testing/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
